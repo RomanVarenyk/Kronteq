d0955bcb41130570cde61fe4753575d1b7526baf
Ukraine1449 <roma.bystriakov@gmail.com>
2021-12-19 19:58:34 -0500

    Added debbuging command

---
+++ src/main/java/com/github/ukraine1449/kronteq/Commands/debugCommand.java
@@ -0,0 +1,43 @@
+package com.github.ukraine1449.kronteq.Commands;
+
+import com.github.ukraine1449.kronteq.Kronteq;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class debugCommand implements CommandExecutor {
+Kronteq plugin;
+
+    public debugCommand(Kronteq plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            player.sendMessage("Que: " + plugin.que);
+            player.sendMessage("Sumo1: " + plugin.sumo1);
+            player.sendMessage("Sumo2: " + plugin.sumo2);
+            player.sendMessage("Sumo3: " + plugin.sumo3);
+            player.sendMessage("Free arenas: " + plugin.freeArenas);
+            player.sendMessage("isDuelFree: " + plugin.isDuelFree);
+            player.sendMessage("duelList: " + plugin.duelList);
+            player.sendMessage("cDuel: " + plugin.cDuel);
+            player.sendMessage("isInCurrentMatch: " + plugin.isInCurrentMatch);
+        }else{
+            System.out.println("Que: " + plugin.que);
+            System.out.println("Sumo1: " + plugin.sumo1);
+            System.out.println("Sumo2: " + plugin.sumo2);
+            System.out.println("Sumo3: " + plugin.sumo3);
+            System.out.println("Free arenas: " + plugin.freeArenas);
+            System.out.println("isDuelFree: " + plugin.isDuelFree);
+            System.out.println("duelList: " + plugin.duelList);
+            System.out.println("cDuel: " + plugin.cDuel);
+            System.out.println("isInCurrentMatch: " + plugin.isInCurrentMatch);
+        }
+        return true;
+    }
+}
--- src/main/java/com/github/ukraine1449/kronteq/Kronteq.java
+++ src/main/java/com/github/ukraine1449/kronteq/Kronteq.java
@@ -50,6 +50,7 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         getServer().getPluginManager().registerEvents(new playerLeaveEvent(this), this);
         getCommand("duel").setExecutor(new Duel(this));
         getServer().getPluginManager().registerEvents(new playerKillEvent(this), this);
+        getCommand("debug").setExecutor(new debugCommand(this));
         getCommand("stats").setExecutor(new checkPlayerStats(this));
         getCommand("joinqueue").setExecutor(new joinQue(this));
         getCommand("getQueue").setExecutor(new getQue(this));
@@ -60,7 +61,7 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         String ip = getConfig().getString("ip");
         String password = getConfig().getString("password");
         String username = getConfig().getString("username");
-        String dbn = getConfig().getString("database name");
+        String dbn = getConfig().getString("database name");//these 4 strings get the login info from config.yml file, and use that for DB connections
         try{
             String driver = "com.mysql.jdbc.Driver";
             String url = "jdbc:mysql://"+ ip + ":3306/" + dbn;
@@ -69,7 +70,9 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected");
             return conn;
-        }catch(Exception e){}
+        }catch(Exception e){
+            System.out.println("Unable to connect to SQL server.");
+        }
         return null;
     }
     public void createTableUserStats()throws Exception{
@@ -77,14 +80,15 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
             Connection con = getConnection();
             PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS userStats(UUID varchar(255),wonGames int,lostGames int,kills int,deaths int, PRIMARY KEY (UUID))");
             create.executeUpdate();
-            con.close();
+            con.close();// create table named userStats, with UUID as a string and also primary ID, along with wonGames, lostGames, kills and deaths as ints
         }catch(Exception e){}
     }
     public void consoleError(String Location){
+        //method called when there is an error with something on the backend of the server
         System.out.println("An error has occured with "+ Location+". Please look at the error code and attempt to troubleshoot, if not possible please contact the author. Discord: Ukraine#1449 Email: ukraine1449@gmail.com");
     }
     public void playerJoinQuery(String UUID){
-        try{
+        try{//executed when a player is joining in playerJoinEvent, basically if the players UUID isnt already in the database it adds it with all stats of 0
             Connection con = getConnection();
             PreparedStatement posted = con.prepareStatement("INSERT INTO userStats(UUID, wonGames, lostGames, kills, deaths) VALUES ('"+UUID+"', 0, 0, 0, 0)ON DUPLICATE KEY UPDATE UUID='"+UUID+"'");
             posted.executeUpdate();
@@ -95,6 +99,8 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         }
     }
     public ArrayList<Integer> selectCD(String UUID) throws Exception {
+        //gets won and lost games along with kills and deaths where UUID is what has been passed in as a string.
+        //returns results as ints in an arraylist
         System.out.println("ch1");
         ArrayList<Integer> retuns = new ArrayList<Integer>();
         Connection con = getConnection();
@@ -113,7 +119,7 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         return retuns;
     }
     public void postOnKill(String UUIDK, String UUIDD) throws Exception {
-     Connection con = getConnection();
+     Connection con = getConnection();//updates the database with won and lost game and kills and deaths. planning to modernize this to use only 2 instead of 4 vars.
      PreparedStatement updateWonGames = con.prepareStatement("UPDATE userStats SET wonGames=+1  WHERE UUID='"+UUIDK+"'");
          PreparedStatement updateLostGames = con.prepareStatement("UPDATE userStats SET lostGames=+1  WHERE UUID='"+UUIDD+"'");
          updateLostGames.executeUpdate();
@@ -125,22 +131,28 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         con.close();
     }
     public void playerError(Player player, String Location){
+        //used when there is some kind of error with a players side. is send to passed in player.
         player.sendMessage(ChatColor.RED+"An error has occured with "+ Location+". Please look at the error code in the console, and attempt to troubleshoot, if not possible please contact the author. Discord: Ukraine#1449 Email: ukraine1449@gmail.com");
     }
     public void playerTeleportToReady(){
         if(que.size() >= 2){
+            //checks if que size is more or equal to 2, then checks if free arenas arent empty
             if(!freeArenas.isEmpty()){
+                //gets a base location named baseloc and sets it as the p1l and p2l locations
                 Location baseloc = que.get(0).getLocation();
                 Bukkit.broadcastMessage(String.valueOf(baseloc));
                 Location p1l = baseloc;
                 Location p2l = baseloc;
+                //gets free arena name, gets the 2 players
                 String arenaName = freeArenas.get(0);
                 Player player1 = que.get(0);
                 Player player2 = que.get(1);
                 if(!isInCurrentMatch.contains(player1) && !isInCurrentMatch.contains(player2)){
+                    //if neither player is in a match add them to the current match list
                 isInCurrentMatch.add(player1);
                 isInCurrentMatch.add(player2);
                 if(arenaName.equals("Sumo2")){
+                    //set world of location name to the appropriate name after which arena has been selected. add them to the list of said arena, remove from que and set the p1l and p2l new coords.
                     World world = getServer().getWorld("Sumo2");
                     sumo2.add(player1);
                     sumo2.add(player2);
@@ -183,6 +195,7 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
                     p2l.setZ(-1);
                     p2l.setWorld(world);
                 }}
+                //telepirt the players to said locations and send them both a message saying the battle has started.
                 player1.teleport(p1l);
                 player2.teleport(p2l);
                 player1.sendMessage(ChatColor.RED + "Your match has started with " + player2.getDisplayName());
@@ -194,12 +207,15 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
         Location hub = getServer().getWorld("Practice").getSpawnLocation();
         p1.teleport(hub);
         p2.teleport(hub);
+        //teleports both players to spawn location of the world called Practice
     }
     public void playerDuelStart(Player p1, Player p2, String args){
         if(isDuelFree){
+            //first checks if isDuelFree is true meaning nobody is dueling now, then checks if its an A to accept, a D to deny or an S to send
             if(args.equals("a")){
                 if(duelList.containsKey(p2)){
                 if (duelList.get(p2).getPlayer().getUniqueId() == p1.getUniqueId()){
+                    //checks if both players are willing to duel, sets duel arena free to false and teleports them adding them to a match
                     isDuelFree = false;
                     Location defloc = p1.getLocation();
                     defloc.setWorld(getServer().getWorld("duels"));
@@ -217,13 +233,17 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
             }else if(args.equals("d")){
                 if(duelList.containsKey(p2)){
                     if (duelList.get(p2).getPlayer().getUniqueId() == p1.getUniqueId()){
+                        //deletes the duel request after check if the duel has actually been sent out and cancels it removing it from the hashmap of requested duels
                         p2.sendMessage(ChatColor.RED + "Your duel request has been denied.");
+                        p1.sendMessage(ChatColor.RED + "You canceled your duel with " + p2.getDisplayName());
                         duelList.remove(p2);
                     }}
             }else if (args.equals("s")){
+                //adds to hash map of requested players and sends both a message
                 duelList.putIfAbsent(p1, p2);
                 p2.sendMessage(p1.getDisplayName() + " " + ChatColor.BLUE + "has invited you to duel. to accept do /duel " + p1.getDisplayName() + " a");
                 p2.sendMessage(ChatColor.BLUE + "To deny the request to duel do /duel " + p1.getDisplayName() + " d");
+                p1.sendMessage(ChatColor.GREEN + "Invited " + p2.getDisplayName() + ChatColor.GREEN + " to a match.");
             }
         }else{
             p1.sendMessage(ChatColor.RED + "The duel arena isnt free. Please try again later.");
@@ -250,4 +270,4 @@ public ArrayList<Player> isInCurrentMatch = new ArrayList<Player>();
             queueSize.setScore(que.size());
         }
     }
-}
+}//TODO add perms and a debug command to output all public major vals
